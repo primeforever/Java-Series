f2a9cc935862ce393554e660c648add46df5c004
primeforever <primeforever@users.noreply.github.com>
2026-08-20 17:10:26 +0530

    q4

---
+++ src/loops_practice/q4.java
@@ -0,0 +1,18 @@
+//Q4))Write a Java program to calculate the sum of numbers from 1 to N (both inclusive).
+
+package loops_practice;
+import java.util.*;
+public class q4 {
+    static void main(){
+        Scanner sc = new Scanner(System.in);
+        int sum =0;
+        System.out.println("enter the number N : ");
+        int n = sc.nextInt();
+        for (int i = 1; i<=n; i++){
+            sum = sum + i;
+        }
+        System.out.println("the final sum is : " + sum);
+
+
+    }
+}
