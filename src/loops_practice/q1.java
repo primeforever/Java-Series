970d110b1a924733159270a985d2590f82bfbd27
primeforever <primeforever@users.noreply.github.com>
2026-08-20 16:28:45 +0530

    q1

---
+++ src/loops_practice/q1.java
@@ -0,0 +1,11 @@
+//Q1))Write a Java program to print numbers from 1 to 10 using a for loop.
+
+package loops_practice;
+
+public class q1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
