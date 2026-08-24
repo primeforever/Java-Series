4c7bae9be2475f25218676d2bb1a3ec8ae50244b
primeforever <primeforever@users.noreply.github.com>
2026-08-24 21:52:25 +0530

    q1

---
+++ src/Conditionals/q1.java
@@ -0,0 +1,11 @@
+package Conditionals;
+
+public class q1 {
+    static void main() {
+
+        int dailyCodes = 20;
+        if(dailyCodes >= 15){
+            System.out.println("Optimised Learning");
+        }
+    }
+}
