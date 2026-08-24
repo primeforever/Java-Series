4f22ce4ab820702dfeae4d5065fec369427099b7
primeforever <primeforever@users.noreply.github.com>
2026-08-24 22:02:51 +0530

    q2

---
+++ src/Conditionals/q2.java
@@ -0,0 +1,16 @@
+//IF_ELSE CONDITION
+
+package Conditionals;
+
+public class q2 {
+    static void main() {
+
+        int Marks = 85;
+        if (Marks >= 33) {
+            System.out.println("pass");
+        } else
+        {
+            System.out.println("Fail");
+        }
+    }
+}
