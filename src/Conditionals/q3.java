5e8be97001595ada3fcba0df8cd8707e13558268
primeforever <primeforever@users.noreply.github.com>
2026-08-25 23:44:42 +0530

    q3

---
+++ src/Conditionals/q3.java
@@ -0,0 +1,36 @@
+//IF_ELSE_IF
+
+package Conditionals;
+import java.util.*;
+public class q3 {
+    static void main() {
+        Scanner Day = new Scanner(System.in);
+        System.out.println("enter a Number Between 1-7 : ");
+        int n = Day.nextInt();
+        if (n == 1) {
+            System.out.println("Monday");
+        }
+        else if(n == 2){
+            System.out.println("Tuesday");
+        }
+        else if(n == 3){
+            System.out.println("Wednesday");
+        }
+        else if(n == 4){
+            System.out.println("Thursday");
+        }
+        else if(n == 5){
+            System.out.println("Friday");
+        }
+        else if(n == 6){
+            System.out.println("Saturday");
+        }
+        else if(n == 7){
+            System.out.println("Sunday");
+        }
+        else{
+            System.out.println("Please Enter a Valid value");
+        }
+
+    }
+}
