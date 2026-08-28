71e73d8a59fd1ff1ebffd99233be66285d7e67fd
primeforever <primeforever@users.noreply.github.com>
2026-08-28 23:30:12 +0530

    q5

---
+++ src/Conditionals/q5.java
@@ -0,0 +1,11 @@
+package Conditionals;
+
+public class q5 {
+    static void main() {
+        int streakDays = 31;
+
+        String status = (streakDays >= 30) ? "Consistent" : "Irregular";
+
+        System.out.println(status);
+    }
+}
