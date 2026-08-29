6a1620f7a1a0238baa3577c30ec16be2ced91ec1
primeforever <primeforever@users.noreply.github.com>
2026-08-29 22:48:52 +0530

    q6

---
+++ src/Conditionals/q6.java
@@ -0,0 +1,25 @@
+package Conditionals;
+
+public class q6 {
+    public static void main(){
+
+        int dayNumber = 3;
+
+        switch (dayNumber) {
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
