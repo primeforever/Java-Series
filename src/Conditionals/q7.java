2e362689c6d865a6048f1ac9819008c5ce113988
primeforever <primeforever@users.noreply.github.com>
2026-08-30 23:13:51 +0530

    q7

---
+++ src/Conditionals/q7.java
@@ -0,0 +1,17 @@
+package Conditionals;
+
+public class q7 {
+    public static void main(String[] args) {
+
+        int level = 2;
+
+        switch (level) {
+            case 1:
+                System.out.println("Beginner");
+            case 2:
+                System.out.println("Intermediate");
+            case 3:
+                System.out.println("Advanced");
+        }
+    }
+}
