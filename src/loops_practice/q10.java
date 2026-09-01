8ce65adb760f0e10f9fe1156e8647e19d306e78f
primeforever <primeforever@users.noreply.github.com>
2026-09-01 23:17:05 +0530

    q10

--- src/loops_practice/q10.java
+++ src/loops_practice/q10.java
@@ -1,4 +1,13 @@
 package loops_practice;
 
 public class q10 {
+    static void main() {
+
+        for (int testCase = 1; testCase <= 6; testCase++) {
+            if (testCase == 4) {
+                break;
+            }
+            System.out.println(testCase);
+        }
+    }
 }
