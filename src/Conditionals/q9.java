1a4be22263e6fa08a5050ef2bde44d7273dce74f
primeforever <primeforever@users.noreply.github.com>
2026-09-01 23:07:28 +0530

    q9

---
+++ src/Conditionals/q9.java
@@ -0,0 +1,8 @@
+package Conditionals;
+
+public class q9 {
+    static void main() {
+        System.out.println("hello world");
+        System.out.println("hii");
+    }
+}
---
+++ src/loops_practice/q9.java
@@ -0,0 +1,16 @@
+//Print a small square pattern
+package loops_practice;
+
+public class q9 {
+    static void main() {
+
+
+        for (int row = 1; row <=3; row++){
+            for (int col = 1; col <= 3; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
