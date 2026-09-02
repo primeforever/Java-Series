4946b04da0fafda8cef9ab00e2c66614ab1c9e5d
primeforever <primeforever@users.noreply.github.com>
2026-09-02 22:47:27 +0530

    q11

---
+++ src/loops_practice/q11.java
@@ -0,0 +1,13 @@
+package loops_practice;
+
+public class q11 {
+    public static void main(String[] args) {
+
+        int revisionRound = 1;
+
+        do {
+            System.out.println("Revision Round: " + revisionRound);
+            revisionRound++;
+        } while (revisionRound <= 3);
+    }
+}
