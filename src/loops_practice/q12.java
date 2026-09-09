21b1dec1c7de3d16dd14e8fd0ab7faff6ddfeae4
primeforever <primeforever@users.noreply.github.com>
2026-09-09 22:40:57 +0530

    q12

---
+++ src/loops_practice/q12.java
@@ -0,0 +1,12 @@
+package loops_practice;
+
+public class q12 {
+    public static void main(String[] args) {
+            for (int testCase = 1; testCase <= 6; testCase++) {
+                if (testCase == 4) {
+                    break;
+                }
+                System.out.println(testCase);
+            }
+        }
+    }
