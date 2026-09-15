693a3d567358adb108edb5a070ce47b4abbbcbaa
primeforever <primeforever@users.noreply.github.com>
2026-09-15 23:23:31 +0530

    q13

--- src/Conditionals/q13.java
+++ src/Conditionals/q13.java
@@ -1,4 +1,23 @@
+//Finding Leap Year
+
 package Conditionals;
 
+import java.util.*;
 public class q13 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
+
+        if (year % 400 == 0) {
+            System.out.println("Leap Year");
+        } else if (year % 100 == 0) {
+            System.out.println("Not a Leap Year");
+        } else if (year % 4 == 0) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Not a Leap Year");
+        }
+    }
 }
