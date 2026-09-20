b9c0404f0205d7c1f98f4815a01d4dc9d2c10b90
primeforever <primeforever@users.noreply.github.com>
2026-09-20 18:39:35 +0530

    Q2

---
+++ src/Methods/Q2.java
@@ -0,0 +1,12 @@
+//Parameters & No Parameters
+
+package Methods;
+
+public class Q2 {
+    static void Printsum(int x, int y){
+        System.out.println("sum= " + (x+y));
+    }
+    static void main() {
+        Printsum(5,10);
+    }
+}
