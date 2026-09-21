c78b7052285f3119f04e65e27ddd7c4266f509e0
primeforever <primeforever@users.noreply.github.com>
2026-09-21 23:50:40 +0530

    Q3

--- src/Methods/Q3.java
+++ src/Methods/Q3.java
@@ -1,4 +1,28 @@
+//Square Of Numbers
+
 package Methods;
 
+import java.util.Scanner;
+
 public class Q3 {
+
+    // Method to calculate square
+    static int calculateSquare(int num) {
+        return num * num;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        // Calling the method
+        int result = calculateSquare(number);
+
+        System.out.println("Square = " + result);
+
+        sc.close();
+    }
 }
