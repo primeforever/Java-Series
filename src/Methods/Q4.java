652e8774b2455fa2a956bb6dc3197ae0db9b81a4
primeforever <primeforever@users.noreply.github.com>
2026-09-22 12:56:43 +0530

    Q4

---
+++ src/Methods/Q4.java
@@ -0,0 +1,12 @@
+//Void Return type
+
+package Methods;
+
+public class Q4 {
+    static void Multiply(int a, int b){
+        System.out.println("Sum=" + (a*b));
+    }
+    static void main(){
+        Multiply(5,10);
+    }
+}
