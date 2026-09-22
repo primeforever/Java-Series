2f66a8d086aef7f477d92ee049ea32ffff00478b
primeforever <primeforever@users.noreply.github.com>
2026-09-22 14:00:13 +0530

    Q6

---
+++ src/Methods/Q6.java
@@ -0,0 +1,22 @@
+//Execution and Workflow Argument
+
+package Methods;
+
+public class Q6 {
+    void main(){
+        System.out.println("Hi");
+        solve();
+        System.out.println("BYe");
+    }
+    void solve(){
+        System.out.println("Hey");
+        int ans = add(15, 10);
+        System.out.println(ans);
+        System.out.println("BYE Bye");
+    }
+    int add(int a, int b){
+        System.out.println("hello");
+        int ans = a+b;
+        return ans;
+    }
+}
