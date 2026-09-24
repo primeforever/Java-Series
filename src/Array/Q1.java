6b970f115d5a6db072b6d9558548b37ddea0343a
primeforever <primeforever@users.noreply.github.com>
2026-09-24 23:43:44 +0530

    Q1

---
+++ src/Array/Q1.java
@@ -0,0 +1,21 @@
+//Creating An ARRAY and Accessing the values
+
+package Array;
+
+public class Q1 {
+    static void main() {
+
+        //declaration
+        int arr[];
+
+        //allocation
+        arr = new int[5];
+
+        //initialization
+        int brr[] ={10, 20, 30};
+
+        System.out.println("Value at 0 index is :" + brr[0]);
+        System.out.println("Value at 1 index is :" + brr[1]);
+        System.out.println("Value at 2 index is :" + brr[2]);
+    }
+}
